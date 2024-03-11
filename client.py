8b5c6d12ec7c29e0f3026dfe0754e37a887a8476
zanadoman <domanseven@gmail.com>
2024-03-11 16:41:30 +0100

    .

--- client.py
+++ client.py
@@ -7,7 +7,11 @@ def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(1)
     while True:
-        sock.sendto(bytes(input(SERVER + ':' + str(PORT) + '> '), 'utf-8'), (SERVER, PORT))
+        try:
+            msg = input(SERVER + ':' + str(PORT) + '> ')
+        except KeyboardInterrupt:
+            exit()
+        sock.sendto(bytes(msg, 'utf-8'), (SERVER, PORT))
         try:
             if sock.recv(1) == b'\x00':
                 print('arrived')
