5ee597f61f2403334a75e4a722d79f19a5235ba3
zanadoman <domanseven@gmail.com>
2024-03-11 16:17:11 +0100

    .

--- client.py
+++ client.py
@@ -5,15 +5,16 @@ PORT = 49152
 
 def main():
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.settimeout(1)
     while True:
+        sock.sendto(bytes(input(SERVER + ':' + str(PORT) + '> '), 'utf-8'), (SERVER, PORT))
         try:
-            sock.sendto(bytes(input(SERVER + ':' + str(PORT) + '> '), 'utf-8'), (SERVER, PORT))
             if sock.recv(1) == b'\x00':
                 print('arrived')
             else:
                 print('lost')
-        except:
-            exit()
+        except socket.timeout:
+            print('timeout')
 
 if __name__ == '__main__':
     main()
